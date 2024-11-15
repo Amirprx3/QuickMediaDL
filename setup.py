6e18c37531f8bf326993df4ff11a08db9a83c877
Amirprx3 <120316482+Amirprx3@users.noreply.github.com>
2024-11-15 21:40:59 +0330

    setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="QuickMediaDL",
-    version="0.1.0",
+    version="0.1.1",
     description="A library for downloading videos or audio from online sources",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
